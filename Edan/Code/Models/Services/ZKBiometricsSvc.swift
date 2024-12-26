6315f83a85c861de8f6dcac4e5ff8c5b39b6502d
ivanlele <ivanlele402@gmail.com>
2024-12-26 19:03:27 +0200

    Refactor ZKBiometricsSvc to remove outdated comments and update deleteValue method signature

--- Edan/Code/Models/Services/ZKBiometricsSvc.swift
+++ Edan/Code/Models/Services/ZKBiometricsSvc.swift
@@ -36,22 +36,6 @@ class ZKBiometricsSvc {
         .get()
     }
 
-//    Delete value from storage. If no filter query specified nothing is delete, because one of filters is required.
-//    When filtering by value, service queries by exact match and if any value found deletes it.
-//    Also take into account that value is Base64 encoded, so may contain some escape symbol that should be encoded.
-//
-//    query Parameters
-//    filter[key]
-//    string
-//    Example: filter[key]=550e8400-e29b-41d4-a716-446655440000
-//    Param to filter values by uuid key
-//
-//    filter[value]
-//    string
-//    Example: filter[value]=aGVsbG8gd29ybGQ=
-//    Param to filter data by Base64 encoded value
-//
-
     func getValue(_ key: String? = nil, _ value: Data? = nil) async throws -> ZKBiometricsValueResponse {
         var requestURL = url
         requestURL.append(path: "integrations/zk-biometrics-svc/value")
@@ -74,7 +58,7 @@ class ZKBiometricsSvc {
         .get()
     }
 
-    func deleteValue(_ key: String?) async throws -> ZKBiometricsValueResponse {
+    func deleteValue(_ key: String? = nil, _ value: Data? = nil) async throws -> ZKBiometricsValueResponse {
         var requestURL = url
         requestURL.append(path: "integrations/zk-biometrics-svc/value")
 
