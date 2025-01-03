f8f1bb9828fe513e3acafd1b8b31279da0254107
ivanlele <ivanlele402@gmail.com>
2025-01-03 17:53:45 +0200

    Add PoseidonHash utility class for hashing data

---
+++ Edan/Code/Utilities/PoseidonHash.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Identity
+
+class PoseidonHash {
+    static func hash(_ data: [Data]) throws -> Data {
+        let poseidonHash = IdentityNewPoseidonHash()
+        guard let poseidonHash else { throw "failed to create PoseidonHash" }
+        
+        for datum in data {
+            poseidonHash.add(datum)
+        }
+        
+        var error: NSError?
+        let result = poseidonHash.hash(&error)
+        if let error {
+            throw error
+        }
+        
+        guard let result else { throw "failed to create PoseidonHash" }
+        
+        return result
+    }
+}
