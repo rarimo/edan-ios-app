6fea177952b797b51fbd59bc34a68fce11669923
ivanlele <ivanlele402@gmail.com>
2025-01-03 17:11:50 +0200

    Refactor WalletManager to improve balance retrieval logic and error handling

--- Edan/Code/Managers/WalletManager.swift
+++ Edan/Code/Managers/WalletManager.swift
@@ -7,7 +7,6 @@ import Foundation
 class WalletManager: ObservableObject {
     static let shared = WalletManager()
 
-    let ownerAddress = "0xa28FD19165438ee002Bf3CE7c4D74484d9f4f0B5"
     let accountAddress = "0x832eceB65fC4c4549cb12CF3dd6bA447915a6244"
 
     let decimals: Int = 18
@@ -37,25 +36,28 @@ class WalletManager: ObservableObject {
             }
 
             do {
-                let web3 = Web3(rpcURL: ConfigManager.shared.general.evmRpcURL.absoluteString)
-
-                let erc20Address = try EthereumAddress(hex: ConfigManager.shared.general.erc20Address, eip55: false)
-
-                let erc20Contract = GenericERC20Contract(address: erc20Address, eth: web3.eth)
-
-                let accountAddress = try EthereumAddress(hex: AccountManager.shared.ethreumAddress, eip55: false)
-
-                let balanceOfResponse = try erc20Contract.balanceOf(address: accountAddress).call().wait()
-
-                guard let balanceValue = balanceOfResponse[""] as? BigUInt else {
-                    LoggerUtil.common.error("failed to fetch balance")
-                    return
-                }
-
-                self.balance = balanceValue
+                self.balance = try await retriveBalance()
             } catch {
                 LoggerUtil.common.error("failed to fetch balance: \(error.localizedDescription)")
             }
         }
+    }
+
+    func retriveBalance() async throws -> BigUInt {
+        let web3 = Web3(rpcURL: ConfigManager.shared.general.evmRpcURL.absoluteString)
+
+        let erc20Address = try EthereumAddress(hex: ConfigManager.shared.general.erc20Address, eip55: false)
+
+        let erc20Contract = GenericERC20Contract(address: erc20Address, eth: web3.eth)
+
+        let accountAddress = try EthereumAddress(hex: AccountManager.shared.ethreumAddress, eip55: false)
+
+        let balanceOfResponse = try erc20Contract.balanceOf(address: accountAddress).call().wait()
+
+        guard let balanceValue = balanceOfResponse["_balance"] as? BigUInt else {
+            throw "Response does not contain balance"
+        }
+
+        return balanceValue
     }
 }
