387eb60527762503abff424f553d0068a2c0b641
ivanlele <ivanlele402@gmail.com>
2025-01-03 15:46:27 +0200

    Add relay functionality to ZKBiometricsSvc for transaction handling

--- Edan/Code/Models/Services/ZKBiometricsSvc.swift
+++ Edan/Code/Models/Services/ZKBiometricsSvc.swift
@@ -106,6 +106,38 @@ class ZKBiometricsSvc {
             throw error
         }
     }
+
+    func relay(
+        _ calldata: Data,
+        _ destination: String,
+        _ noSend: Bool = false
+    ) async throws -> ZKBiometricsRelayResponse {
+        var requestURL = url
+        requestURL.append(path: "integrations/zk-biometrics-svc/relay")
+
+        let request = ZKBiometricsRelayRequest(
+            data: .init(
+                id: "",
+                type: "txs",
+                attributes: .init(
+                    destination: destination,
+                    noSend: noSend,
+                    txData: calldata.fullHex
+                )
+            )
+        )
+
+        return try await AF.request(
+            requestURL,
+            method: .post,
+            parameters: request,
+            encoder: JSONParameterEncoder.default
+        )
+        .validate(OpenApiError.catchInstance)
+        .serializingDecodable(ZKBiometricsRelayResponse.self)
+        .result
+        .get()
+    }
 }
 
 struct ZKBiometricsAddValueRequest: Codable {
@@ -146,4 +178,42 @@ struct ZKBiometricsValueResponseData: Codable {
 struct ZKBiometricsValueResponseAttributes: Codable {
     let key: String
     let feature: [Double]
+}
+
+struct ZKBiometricsRelayRequest: Codable {
+    let data: ZKBiometricsRelayRequestData
+}
+
+struct ZKBiometricsRelayRequestData: Codable {
+    let id, type: String
+    let attributes: ZKBiometricsRelayRequestAttributes
+}
+
+struct ZKBiometricsRelayRequestAttributes: Codable {
+    let destination: String
+    let noSend: Bool
+    let txData: String
+
+    enum CodingKeys: String, CodingKey {
+        case destination
+        case noSend = "no_send"
+        case txData = "tx_data"
+    }
+}
+
+struct ZKBiometricsRelayResponse: Codable {
+    let data: ZKBiometricsRelayResponseData
+}
+
+struct ZKBiometricsRelayResponseData: Codable {
+    let id, type: String
+    let attributes: ZKBiometricsRelayResponseAttributes
+}
+
+struct ZKBiometricsRelayResponseAttributes: Codable {
+    let txHash: String
+
+    enum CodingKeys: String, CodingKey {
+        case txHash = "tx_hash"
+    }
 }
