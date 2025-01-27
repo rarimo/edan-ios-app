24a66554da68fd0e9a0ead83453bc912dce7a629
ivanlele <ivanlele402@gmail.com>
2025-01-27 18:55:26 +0200

    Add SetupCreateNewIntroView and associated assets for onboarding

--- Edan/Code/Constants/Images.swift
+++ Edan/Code/Constants/Images.swift
@@ -3,4 +3,5 @@ import Foundation
 class Images {
     static let passportFrame = "PassportFrame"
     static let setup = "Setup"
+    static let setupFirstIntro = "SetupFirstIntro"
 }
---
+++ Edan/Code/Modules/Setup/Views/SetupCreateNewIntroView.swift
@@ -0,0 +1,72 @@
+import SwiftUI
+
+private enum IntroTab: Int {
+    case first
+    case second
+}
+
+struct SetupCreateNewIntroView: View {
+    @State private var selectedTab: IntroTab = .first
+
+    var onFinish: () -> Void
+
+    var body: some View {
+        VStack {
+            Spacer()
+            TabView(selection: $selectedTab) {
+                firstIntroBody
+                    .tag(IntroTab.first)
+                secontIntroBody
+                    .tag(IntroTab.second)
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .frame(height: 450)
+            tabIndicators
+            Spacer()
+            VStack(spacing: 32) {
+                Divider()
+                AppButton(text: "Create an unforgettable wallet", action: onFinish)
+            }
+            .frame(width: 342)
+        }
+    }
+
+    var firstIntroBody: some View {
+        VStack(spacing: 70) {
+            Image(Images.setupFirstIntro)
+            Text("No more seed\nphrases")
+                .h4()
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.textPrimary)
+        }
+    }
+
+    var secontIntroBody: some View {
+        VStack(spacing: 70) {
+            VStack {
+                Text("It’s just You")
+                    .h4()
+                Text("+ ZK Face  for managing all")
+                    .subtitle2()
+            }
+        }
+    }
+
+    var tabIndicators: some View {
+        HStack {
+            tabIndicator(selectedTab == .first)
+            tabIndicator(selectedTab == .second)
+        }
+        .animation(.smooth, value: selectedTab)
+    }
+
+    func tabIndicator(_ isActive: Bool) -> some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(width: isActive ? 16 : 8, height: 8)
+            .foregroundStyle(isActive ? .primaryMain : .componentPrimary)
+    }
+}
+
+#Preview {
+    SetupCreateNewIntroView {}
+}
