d6f27835b3688e38e62d21bdf28b3a0f37555fe1
ivanlele <ivanlele402@gmail.com>
2025-01-28 16:15:37 +0200

    Refactor AppView to remove commented-out code and simplify structure; add new localizable strings for user prompts

--- Edan/Code/Modules/App/Views/AppView.swift
+++ Edan/Code/Modules/App/Views/AppView.swift
@@ -1,39 +1,40 @@
 import SwiftUI
 
 struct AppView: View {
-    @EnvironmentObject private var internetConnectionManager: InternetConnectionManager
+//    @EnvironmentObject private var internetConnectionManager: InternetConnectionManager
 
-    @StateObject var viewModel = ViewModel()
+//    @StateObject var viewModel = ViewModel()
 
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            if !internetConnectionManager.isInternetPresent {
-                InternetConnectionRequiredView()
-            } else if !viewModel.isPassportPresent && viewModel.isIntroFinished {
-                ScanPassportView(
-                    onComplete: { passport in
-                        try? AppKeychain.setValue(.passportJson, passport.json)
-
-                        viewModel.isPassportPresent = true
-                    },
-                    onClose: {
-                        viewModel.isPassportPresent = true
-                    }
-                )
-            } else if viewModel.isIntroFinished {
-                MainView()
-            } else {
-                IntroView {
-                    viewModel.isIntroFinished = true
-                }
+//        ZStack(alignment: .topLeading) {
+//            if !internetConnectionManager.isInternetPresent {
+//                InternetConnectionRequiredView()
+//            } else if !viewModel.isPassportPresent && viewModel.isIntroFinished {
+//                ScanPassportView(
+//                    onComplete: { passport in
+//                        try? AppKeychain.setValue(.passportJson, passport.json)
+//
+//                        viewModel.isPassportPresent = true
+//                    },
+//                    onClose: {
+//                        viewModel.isPassportPresent = true
+//                    }
+//                )
+//            } else if viewModel.isIntroFinished {
+//                MainView()
+//            } else {
+//                IntroView {
+//                    viewModel.isIntroFinished = true
+//                }
+//            }
+//            AlertManagerView()
+//        }
+//        .background(Color.backgroundPure)
+//        .environmentObject(viewModel)
+        SetupView()
+            .onAppear {
+                LoggerUtil.common.info("App started")
             }
-            AlertManagerView()
-        }
-        .background(Color.backgroundPure)
-        .environmentObject(viewModel)
-        .onAppear {
-            LoggerUtil.common.info("App started")
-        }
     }
 }
 
