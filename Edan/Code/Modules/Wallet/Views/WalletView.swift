797dd41e663e7bf42ec1c2b3693e305a5f9a5046
ivanlele <ivanlele402@gmail.com>
2025-01-29 15:21:30 +0200

    Enhance WalletView with gradient background and action buttons; update arrow icons with new vector assets

--- Edan/Code/Modules/Wallet/Views/WalletView.swift
+++ Edan/Code/Modules/Wallet/Views/WalletView.swift
@@ -2,9 +2,45 @@ import SwiftUI
 
 struct WalletView: View {
     var body: some View {
-        VStack {
-            
+        wrapInGradient {
+            VStack {
+                WarningView(text: "ZK Face technology is currently in beta, with balance limits in place")
+                Spacer()
+                actions
+            }
         }
+    }
+
+    func wrapInGradient(_ body: () -> some View) -> some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(Gradients.primary)
+                    .ignoresSafeArea()
+                body()
+            }
+        }
+    }
+
+    var actions: some View {
+        HStack {
+            action(name: "Receive", iconName: Icons.arrowDown) {}
+            action(name: "Send", iconName: Icons.arrowUp) {}
+        }
+    }
+
+    func action(name: String, iconName: String, action: () -> Void) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 32)
+                .foregroundStyle(.componentPrimary)
+            VStack(spacing: 20) {
+                Image(iconName)
+                Text(name)
+                    .subtitle4()
+                    .foregroundStyle(.textPrimary)
+            }
+        }
+        .frame(width: 177, height: 120)
     }
 }
 
