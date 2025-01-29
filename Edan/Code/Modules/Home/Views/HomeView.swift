6c69d208c75375afe36f6de1dde8dece306ee32f
ivanlele <ivanlele402@gmail.com>
2025-01-29 18:01:37 +0200

    Add tab navigation to HomeView for Wallet and Profile sections

--- Edan/Code/Modules/Home/Views/HomeView.swift
+++ Edan/Code/Modules/Home/Views/HomeView.swift
@@ -1,13 +1,31 @@
 import SwiftUI
 
+private enum HomeTabs: Int {
+    case wallet
+    case profile
+}
+
 struct HomeView: View {
+    @State private var selectedTab: HomeTabs = .wallet
+
     var body: some View {
-        VStack {
-            
+        TabView(selection: $selectedTab) {
+            WalletView()
+                .tag(HomeTabs.wallet)
+            VStack {}
+                .tag(HomeTabs.profile)
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    HomeView()
+    let userManager = UserManager.shared
+
+    userManager.userFace = UIImage(named: Images.man)
+
+    return HomeView()
+        .environmentObject(WalletManager.shared)
+        .environmentObject(userManager)
 }
