1d044b06b6a51efa8cb285393822ea2442ac44d7
ivanlele <ivanlele402@gmail.com>
2025-02-13 15:55:50 +0200

    Bump marketing version to 1.0.2 and add restart features button in CheatView

--- Edan/Code/Modules/Profile/Views/CheatView.swift
+++ Edan/Code/Modules/Profile/Views/CheatView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct CheatView: View {
+    @EnvironmentObject private var appViewModel: AppView.ViewModel
+
     @EnvironmentObject private var walletManager: WalletManager
 
     @State private var isTokensMinting = false
@@ -11,11 +13,38 @@ struct CheatView: View {
                 .foregroundStyle(.textPrimary)
                 .h4()
             Spacer()
+            AppButton(text: "Restart features", action: delete)
             AppButton(text: "Request tokens from the main wallet", action: mintTokens)
                 .isLoading(isTokensMinting)
         }
         .padding()
         .presentationDetents([.fraction(0.25)])
+    }
+
+    func logout() {
+        do {
+            AppUserDefaults.shared.reset()
+
+            appViewModel.reset()
+
+            try AppKeychain.removeAll()
+        } catch {
+            LoggerUtil.common.error("Failed to logout: \(error.localizedDescription)")
+        }
+    }
+
+    func delete() {
+        Task { @MainActor in
+            guard let features = try? JSONDecoder().decode([Double].self, from: AppUserDefaults.shared.faceFeatures) else {
+                logout()
+
+                return
+            }
+
+            try? await ZKBiometricsSvc.shared.deleteValue(feature: features)
+
+            logout()
+        }
     }
 
     func mintTokens() {
@@ -45,4 +74,5 @@ struct CheatView: View {
     VStack {}
         .sheet(isPresented: .constant(true), content: CheatView.init)
         .environmentObject(WalletManager.shared)
+        .environmentObject(AppView.ViewModel())
 }
